880b27bd926f8b92a82752504ffe059534171277
Afrozefathima <jsafroze@gmail.com>
2025-12-15 01:51:43 +0400

    commit

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Footer from "../components/footer"
+import Analytics from '../components/Analytics.client';
+import Tawk from '../components/Tawk.client';
 
 export const viewport = {
   width: 'device-width',
@@ -379,69 +381,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script
-        id="myscript6"
-        dangerouslySetInnerHTML={{
-          __html: `https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0-rc/js/adminlte.min.js`,
-        }}
-        integrity="sha512-pbrNMLSckfh8yEOr2o1RT+4zMU3Sj7+zP3BOY6nFVI/FLnjTRyubNppLbosEt4nvLCcdsEa8tmKhH3uqOYFXKg=="
-        crossOrigin="anonymous"
-        referrerPolicy="no-referrer"
-      />
-      <Script
-        id="myscript5"
-        src="https://polyfill.io/v3/polyfill.min.js?features=default"
-        async
-      ></Script>
-      <Script
-        language="javascript"
-        src={`https://www.exactseek.com/remote-submit.js`}
-      />
-
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/6210ae28a34c2456412714f6/1fs8igkmc';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-              })();
-              `,
-        }}
-      ></script>
-      <script
-        id="myscript"
-        dangerouslySetInnerHTML={{
-          __html: `
-              (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "c2136u1t6f");
-              `,
-        }}
-      />
-      <script
-        id="myscript2"
-        dangerouslySetInnerHTML={{
-          __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-3E8C09YD12', {
-                  page_path: window.location.pathname,
-                });
-              `,
-        }}
-      />
-      <script
-        id="myscript3"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=G-3E8C09YD12`}
-      />
-
       <body className="font-sans">
+
+        <Analytics />
+        <Tawk />
         <Navbar />
         {children}
         <Footer />
