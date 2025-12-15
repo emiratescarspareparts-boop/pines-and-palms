f1bb207cb58a50e1c7bf4387a1bf1cc99aa454d6
Afrozefathima <jsafroze@gmail.com>
2025-12-15 09:35:10 +0400

    commit

--- middleware.js
+++ middleware.js
@@ -91,5 +91,7 @@ export function middleware(request) {
    ========================= */
 
 export const config = {
-    matcher: ['/api/:path*'],
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js)$).*)',
+    ],
 };
