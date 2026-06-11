a3ac85820407aedb3933b0ddbf36b8f327963024
Afrozefathima <jsafroze@gmail.com>
2026-06-11 23:36:30 +0400

    commit

--- app/Removals/page.js
+++ app/Removals/page.js
@@ -8,7 +8,7 @@ export default function Removals() {
         <div><DeadUrlsViewer />
             <div className='sm:max-w-xl lg:max-w-2xl md:max-w-xl xl:max-w-2xl xxl:max-w-2xl mx-auto xs:mx-3 xxs:mx-3 sm:mx-5'>
                 <FormRender formsData={modelforms} postFilter={partsposts} page={`/`} />
-            </div>
+            </div> this is a forbidden pages
         </div>
     )
 }
