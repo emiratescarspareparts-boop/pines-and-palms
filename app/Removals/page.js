30148ba49ba31abbee0be880c2957df0a26b8bcc
Afrozefathima <jsafroze@gmail.com>
2026-06-11 23:34:03 +0400

    commit

--- app/Removals/page.js
+++ app/Removals/page.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import DeadUrlsViewer from '../search-by-make/[make]/[model]/[category]/[subcategory]/Removals'
 import FormRender from "../../components/FormRender"
-
+import CarData from "../../public/lib/car-data.json"
 export default function Removals() {
+    const modelsform = CarData;
     return (
         <div><DeadUrlsViewer />
             <div className='sm:max-w-xl lg:max-w-2xl md:max-w-xl xl:max-w-2xl xxl:max-w-2xl mx-auto xs:mx-3 xxs:mx-3 sm:mx-5'>
